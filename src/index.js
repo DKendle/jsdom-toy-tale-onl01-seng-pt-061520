58aaf547de85b54c9bf79ff84dad4fe12c243e98
DKendle <dina.kendle@gmail.com>
2021-01-05 16:19:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -11,5 +11,28 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+  });  
+  })
+  let tcol = document.getElementById("toy-collection")
+  let div = document.createElement("div")
+  let h2 = document.createElement("h2")
+  let p = document.createElement("p")
+  let img = document.createElement("img")
+
+  fetch("http://localhost:3000/toys")
+  .then(function(response){
+   return response.json()
+  })
+  .then(function(object){
+    doc.appendChild(div)
+    div.className = "card"
+    div.appendChild(h2)
+    h2.innerHTML = object.name
+    div.appendChild(img)
+    div.appendChild(p)
+   
+    
+    img.src = object.image
+    p.innerHTML = object.likes
+
   });
-});
